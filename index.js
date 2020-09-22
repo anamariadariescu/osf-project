41d56509abd1770ead031ba4e55a30ae7cd49f2b
anamariadariescu <anamaria.dariescu@gmail.com>
2020-09-22 16:18:46 +0300

    featured products

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
-    $('.pass_show').append('<span toggle="#input-pwd" class="ptxt fa fa-fw fa-eye field-icon toggle-password"></span>');  
+    $('.pass_show').append('<span toggle="#input-pwd" class="ptxt fa fa-fw fa-eye field-icon toggle-password"></span>');
+    $('.your-class').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        prevArrow: $('.prev1'),
+        nextArrow: $('.next1')
+      });
+    
     });
       
     
@@ -12,3 +21,6 @@ $(document).ready(function(){
     $(this).prev().attr('type', function(index, attr){return attr == 'password' ? 'text' : 'password'; }); 
     
     });  
+
+    // Featured products
+
