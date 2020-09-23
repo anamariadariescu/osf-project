a9f31abfcde3c78bac8538013bc404f23d713329
anamariadariescu <anamaria.dariescu@gmail.com>
2020-09-23 12:09:55 +0300

    benefits content

--- index.js
+++ index.js
@@ -13,14 +13,9 @@ $(document).ready(function(){
       
     
     $(document).on('click','.pass_show .ptxt', function(){ 
-
-        // $(this).text($(this).text() == "Show" ? "Hide" : "Show"); 
-    
-    // $(this).text($(this).text() == "Show" ? "Hide" : "Show"); 
     
     $(this).prev().attr('type', function(index, attr){return attr == 'password' ? 'text' : 'password'; }); 
     
     });  
 
-    // Featured products
 
